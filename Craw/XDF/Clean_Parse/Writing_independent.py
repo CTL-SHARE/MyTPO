d3bc299db64e4fe237b8a7fa0ca7e85ccdc4c294
congtailai <Tailai.Cong@outlook.com>
2024-07-25 10:42:08 +0800

    Finish parsing these parts.

---
+++ Craw/XDF/Clean_Parse/Writing_independent.py
@@ -0,0 +1,25 @@
+import re
+
+import psycopg2
+from bs4 import BeautifulSoup as bs
+
+conn = psycopg2.connect(host="localhost", port="5432", dbname="tpo", user="", password="")
+cur = conn.cursor()
+cur.execute("SELECT examid, full_html FROM xdf.writing_independent")
+rows = cur.fetchall()
+
+for row in rows:
+    html = bs(row[1], "html.parser")
+
+    caption_html = html.find('h3', {'class': 'ul-title'})
+    caption_clean = re.sub(r'\s+', ' ', caption_html.text).strip()
+
+    prompt_html = html.find('div', {'class': 'QandA'})
+    prompt_clean = prompt_html.find('div', {'class': None}).text.replace("  ", "").replace("\n\n", "").strip()
+
+    cur.execute(
+        "UPDATE tpo.xdf.writing_independent SET caption_html = %s, caption_clean = %s, passage_html = %s, passage_clean = %s WHERE examid = %s",
+        (str(caption_html), caption_clean, str(prompt_html), prompt_clean, int(int(row[0]))))
+
+conn.commit()
+conn.close()
---
+++ Craw/XDF/Clean_Parse/Writing_integrated.py
@@ -0,0 +1,35 @@
+import re
+
+import psycopg2
+from bs4 import BeautifulSoup as bs
+
+conn = psycopg2.connect(host="localhost", port="5432", dbname="tpo", user="", password="")
+cur = conn.cursor()
+cur.execute("SELECT examid, full_html FROM xdf.writing_integrated")
+rows = cur.fetchall()
+
+for row in rows:
+    html = bs(row[1], "html.parser")
+
+    caption_html = html.find('h3', {'class': 'ul-title'})
+    caption_clean = re.sub(r'\s+', ' ', caption_html.text).strip()
+
+    passage_html = html.find('div', {'class': 'en'})
+    passage_clean = passage_html.prettify()
+
+    audio_url = [i['src'] for i in html.find_all('audio', {'id': 'listenAudio'})]
+
+    if "暂无原文" in html.find('div', {'role': 'tabpanel'}).text:
+        transcription = None
+    else:
+        transcription = html.find('div', {'role': 'tabpanel'}).text.replace("\n\n", "").replace("\t", ": ").replace(
+            "  ", "").strip()
+
+    cur.execute(
+        "UPDATE xdf.writing_integrated SET caption_html = %s, caption_clean = %s, passage_html = %s, passage_clean = %s, audio_url = %s, audio_download = false, transcription = %s WHERE examid = %s",
+        (
+            str(caption_html), caption_clean, str(passage_html), passage_clean, audio_url, transcription,
+            str(int(row[0]))))
+
+conn.commit()
+conn.close()
