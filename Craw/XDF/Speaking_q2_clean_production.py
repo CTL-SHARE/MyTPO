344a6f3bb718750a40f596cd5050d7f34921c524
congtailai <Tailai.Cong@outlook.com>
2024-08-06 00:12:51 +0800

    Clean passage data of speaking

---
+++ Craw/XDF/Speaking_q2_clean_production.py
@@ -0,0 +1,20 @@
+import psycopg2
+from bs4 import BeautifulSoup as bs
+
+conn = psycopg2.connect(host="localhost", port="5432", dbname="tpo", user="", password="")
+cur = conn.cursor()
+
+cur.execute("SELECT passage_clean, url FROM tpo.xdf.speaking_q2 where passage_json is null")
+rows = cur.fetchall()
+
+for row in rows:
+    passage = bs(row[0], "html.parser")
+    url = row[1]
+
+    passage_json = []
+    for i in passage.find_all()[1:]:
+        if i.get_text(strip=True) != '':
+            passage_json.append(i.get_text(strip=True))
+
+    cur.execute("UPDATE tpo.xdf.speaking_q2 SET passage_json = %s WHERE url = %s", (str(passage_json), url))
+    conn.commit()
