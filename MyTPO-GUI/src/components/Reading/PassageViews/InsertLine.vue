a9ba7ba9b406576703e77756115f87cc032eccec
congtailai <Tailai.Cong@outlook.com>
2024-08-08 18:20:32 +0800

    Add auto scrolling, fix styling

--- MyTPO-GUI/src/components/Reading/PassageViews/InsertLine.vue
+++ MyTPO-GUI/src/components/Reading/PassageViews/InsertLine.vue
@@ -1,18 +1,19 @@
 <template>
   <q-scroll-area ref="scrollArea" class="q-pa-md" style="height: 100%">
-    <div v-for="(item, index) in passage">
-      <h4 v-if="index === 0" align="center" class="q-my-sm">
-        {{ item }}
-      </h4>
+    <div ref="parent">
+      <div v-for="(item, index) in passage">
+        <h4 v-if="index === 0" align="center" class="q-my-sm">
+          {{ item }}
+        </h4>
 
-      <p v-else-if="item.length === 1">
-        <span class="q-px-sm q-mx-sm"></span>
-        {{ item[0] }}
-      </p>
+        <p v-else-if="item.length === 1">
+          <span class="q-px-sm q-mx-sm"></span>
+          {{ item[0] }}
+        </p>
 
-      <p v-else id="insertP">
-        <span class="q-px-sm q-mx-xs text-bold bg-secondary rounded-borders text-white">➜</span>
-        <span v-for="i in item">
+        <p v-else id="insertP">
+          <span ref="targetElement" class="q-px-sm q-mx-xs text-bold bg-secondary rounded-borders text-white">➜</span>
+          <span v-for="i in item">
           <span v-if="i.startsWith('@#')" :id="'insertSpan'+(selection === i[3] ? ' selectedSpan' : '')"
                 class="q-px-sm q-mx-xs text-bold"
                 @click="(selection === i[3]) ? (selection = []) : (selection = i[3]) ; $emit('change-val')">
@@ -20,7 +21,8 @@
           </span>
           <span v-else>{{ i }}</span>
         </span>
-      </p>
+        </p>
+      </div>
     </div>
   </q-scroll-area>
 </template>
@@ -40,7 +42,32 @@ export default {
     insert_txt: String
   },
 
-  methods: {}
+  methods: {
+    autoScroll() {
+      const target = this.$refs.targetElement[0]
+      const parent = this.$refs.parent;
+      if (target && parent) {
+        const elementRect = target.getBoundingClientRect();
+        const parentRect = parent.getBoundingClientRect();
+        const topPercentage = ((elementRect.top - parentRect.top) / parentRect.height);
+        this.$refs.scrollArea.setScrollPercentage('vertical', topPercentage * 2, 200)
+      }
+    }
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      this.autoScroll();
+    });
+  },
+
+  watch: {
+    passage() {
+      this.$nextTick(() => {
+        this.autoScroll();
+      });
+    }
+  }
 }
 </script>
 
--- MyTPO-GUI/src/components/Reading/PassageViews/NormalPassage.vue
+++ MyTPO-GUI/src/components/Reading/PassageViews/NormalPassage.vue
@@ -1,26 +1,28 @@
 <template>
   <q-scroll-area ref="scrollArea" class="q-pa-md" style="height: 100%">
-    <div v-for="(item, index) in passage" style="text-align: justify;">
-      <h4 v-if="index === 0" align="center" class="q-my-sm">
-        {{ item }}
-      </h4>
+    <div ref="parent">
+      <div v-for="(item, index) in passage" style="text-align: justify;">
+        <h4 v-if="index === 0" align="center" class="q-my-sm">
+          {{ item }}
+        </h4>
 
-      <p v-else-if="item.length === 1">
+        <p v-else-if="item.length === 1">
         <span v-if="index === paragraph_idx"
-              class="q-px-sm q-mx-xs text-bold bg-secondary rounded-borders text-white">➜</span>
-        <span v-else class="q-px-sm q-mx-sm"></span>
-        <span>{{ item[0] }}</span>
-      </p>
+              ref="targetElement" class="q-px-sm q-mx-xs text-bold bg-secondary rounded-borders text-white">➜</span>
+          <span v-else class="q-px-sm q-mx-sm"></span>
+          <span>{{ item[0] }}</span>
+        </p>
 
-      <p v-else ref="targetElement">
-        <span class="q-px-sm q-mx-xs text-bold bg-secondary rounded-borders text-white">➜</span>
-        <span v-for="i in item">
+        <p v-else>
+          <span ref="targetElement" class="q-px-sm q-mx-xs text-bold bg-secondary rounded-borders text-white">➜</span>
+          <span v-for="i in item">
           <span v-if="i.startsWith('@#')">
-            <span id="span" class="q-px-sm q-mx-xs q-py-xs text-bold">{{ i.replace('@#', '') }}</span>
+            <span id="span" class="text-bold">{{ i.replace('@#', '') }}</span>
           </span>
           <span v-else>{{ i }}</span>
         </span>
-      </p>
+        </p>
+      </div>
     </div>
   </q-scroll-area>
 </template>
@@ -36,11 +38,36 @@ export default {
   },
 
   props: {
-    passage: String,
-    paragraph_idx: -1
+    passage: Array, // Changed to Array as you're iterating over it
+    paragraph_idx: Number // Changed to Number for consistency
   },
 
-  methods: {}
+  methods: {
+    autoScroll() {
+      const target = this.$refs.targetElement[0]
+      const parent = this.$refs.parent;
+      if (target && parent) {
+        const elementRect = target.getBoundingClientRect();
+        const parentRect = parent.getBoundingClientRect();
+        const topPercentage = ((elementRect.top - parentRect.top) / parentRect.height);
+        this.$refs.scrollArea.setScrollPercentage('vertical', topPercentage * 2, 200)
+      }
+    }
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      this.autoScroll();
+    });
+  },
+
+  watch: {
+    passage() {
+      this.$nextTick(() => {
+        this.autoScroll();
+      });
+    }
+  }
 }
 </script>
 
@@ -59,6 +86,8 @@ export default {
   border-radius: 3px
   background-color: $secondary
   color: white
-
+  -webkit-box-decoration-break: clone
+  box-decoration-break: clone
+  padding: 4px
 
 </style>
